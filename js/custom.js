7fe096e8fd3ae4c8c3d45cd039a0f4adbf2b32b6
ZubairShovan <82607108+ZubairShovan@users.noreply.github.com>
2021-06-01 03:22:24 -0400

    Added current page highlighting

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+	$(function(){
+
+		$('a').each(function(){
+			var hr = $(this).prop('href')
+			if (hr === window.location.href) {
+				console.log($(this).prop('href'))
+				console.log(window.location.href)
+				$(this).addClass('active-page');
+				$(this).parents('li').addClass('active-page');
+			}
+			else if (hr + "/" === window.location.href) {
+				console.log($(this).prop('href'))
+				console.log(window.location.href)
+				$(this).addClass('active-page');
+				$(this).parents('li').addClass('active-page');
+			}
+		});
+	});
+
+
 	$(".nav_ico").click(function(){
 		$(".main_nav").toggleClass("active");
 	});
@@ -7,6 +27,6 @@ $(document).ready(function() {
 	});
 	$( ".main_nav ul" ).click(function( event ) {
 		event.stopImmediatePropagation();
-	});	
+	});
 });
 
